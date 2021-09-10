ac19f74bb6dcb4aa181a9f3ecd479a67c45c1433
Antoine Tavant <antoine.tavant@lpp.polytechnique.fr>
2021-09-10 10:55:54 +0200

    started adapted the learntool for the M2 TP

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-import learntools
+import TPM2PPF_learntools
 from setuptools import setup
 from setuptools import find_packages
 
 setup(name='learntools',
-      version=learntools.__version__,
-      description='Utilities for Kaggle Learn exercises',
+      version=TPM2PPF_learntools.__version__,
+      description='Utilities for The TP of the PPF Master 2, adapted from Kaggle Learn exercises',
       url='http://github.com/kaggle/learntools',
-      author='Dan Becker',
-      author_email='dan@kaggle.com',
+      author='Antoine Tavant',
+      author_email='antoine.tavant@polytechnique.edu',
       license='Apache 2.0',
       packages=find_packages(),
       zip_safe=True)
